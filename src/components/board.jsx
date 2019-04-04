d08b17f6fdd6c0cb475f1d96cc1ebe7ed5a1fbf7
John Botelho <john.botelho@gmail.com>
2019-04-04 15:05:59 -0700

    Refactored board component to use two loops for creation

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -5,31 +5,39 @@ class Board extends Component {
   renderSquare(i) {
     return (
       <Square
+        key={i}
         value={this.props.squares[i]}
         onClick={() => this.props.onClick(i)}
       />
     );
   }
 
+  createBoard() {
+    let board = [];
+    let rows = 3;
+    let cols = 3;
+    let square = 0;
+
+    for (let row = 0; row < rows; row++) {
+      const columns = [];
+      for (let col = 0; col < cols; col++) {
+        columns.push(this.renderSquare(square++));
+      }
+      board.push(
+        <div key={row} className="board-row">
+          {columns}
+        </div>
+      );
+    }
+
+    return board;
+  }
+
   render() {
     return (
       <div>
         <div className="status" />
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {this.createBoard()}
       </div>
     );
   }
